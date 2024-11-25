1402adf4f7ec13b0c5b3096157c7c2630d1c63e1
Demian Samus <demiansamus2@gmail.com>
2024-11-25 20:31:33 +0200

    Added fixtures and updated cors settings

--- sddkd/settings.py
+++ sddkd/settings.py
@@ -123,7 +123,10 @@ MIDDLEWARE = [
     "allauth.account.middleware.AccountMiddleware",
 ]
 
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+]
 
 ROOT_URLCONF = "sddkd.urls"
 
